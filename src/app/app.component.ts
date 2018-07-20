e58207453423c20f6828447e77bc9a16932f3280
Francisco da Silva Filho <francisco.filho@nareia.com.uy>
2018-07-20 18:53:56 -0300

    integracao firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,7 @@ export class MyApp {
   //Essa linha ativa o login do auth0
   //rootPage: any = 'LoginPage';
   rootPage: any = 'MenuPage';
+  token_firebase: any;
 
   constructor(public platform: Platform,
       public statusBar: StatusBar,
@@ -28,62 +29,64 @@ export class MyApp {
       private smartAudioProvider: SmartAudioProvider,
       private firebase: Firebase,
       public loadingController: LoadingController
-    ) {        
-    this.initializeApp();  
+    ) {    
+        let loader = this.loadingController.create({
+          content: "Carregando"
+        }); 
+
+        loader.present();     
+        this.initializeApp(); 
+        loader.dismiss();  
   }
 
-  initializeApp() {
-    //var token = window.localStorage.getItem('id_token');
-    //var expiresAt = JSON.parse(window.localStorage.getItem('expires_at'));   
-    //if(token!=null || (Date.now() < expiresAt)){
-    //  this.rootPage = 'MenuPage';
-    //}
+  
 
-    
+  initializeApp() {  
 
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
-      this.splashScreen.hide();      
-
-      let params= {
-        token : this.device.uuid        
-      };
-
-      let loader = this.loadingController.create({
-        content: "Carregando"
-      }); 
+      this.splashScreen.hide();   
       
-      loader.present(); 
-      this.http.post(CONFIG.url_api+'getId', params, 
-        {
-          headers: { 'Content-Type': 'application/json' }
-        })
-        .toPromise().then(data => {          
-          let token_usuario = JSON.stringify(data[0].token);     
-          let id_usuario = JSON.stringify(data[0].id);
-          window.localStorage.setItem('token_usuario', token_usuario);
-          window.localStorage.setItem('id_usuario', id_usuario); 
-          loader.dismiss();         
-        }).catch(error => {
-          console.log(error.status);
-        }); 
+      //get firebase token
+      this.firebase.getToken()
+      .then(token => {this.getLogin(token)}) // save the token server-side and use it to push notifications to this device
+      .catch(error => console.error('Error getting token', error));      
 
       // Redirect back to app after authenticating
       (window as any).handleOpenURL = (url: string) => {
         Auth0Cordova.onRedirectUri(url);
-      }
-
-      this.firebase.getToken()
-      .then(token => console.log(`The token is ${token}`)) // save the token server-side and use it to push notifications to this device
-      .catch(error => console.error('Error getting token', error));
+      }      
     
       this.firebase.onTokenRefresh()
         .subscribe((token: string) => console.log(`Got a new token ${token}`));
-
-
      
       });      
   }
+
+  getLogin(token: any) {
+    console.log(token);
+    this.token_firebase = token; 
+    //params to get the user token and save firebase token
+    let params= {
+      token : this.device.uuid,
+      token_firebase: this.token_firebase         
+    };
+    
+    this.http.post(CONFIG.url_api+'getId', params, 
+      {
+        headers: { 'Content-Type': 'application/json' }
+      })
+      .toPromise().then(data => {          
+        let token_usuario = JSON.stringify(data[0].token);     
+        let id_usuario = JSON.stringify(data[0].id);
+        window.localStorage.setItem('token_usuario', token_usuario);
+        window.localStorage.setItem('id_usuario', id_usuario);          
+      }).catch(error => {
+        console.log(error.status);
+      }); 
+  }
+
+  
 }
